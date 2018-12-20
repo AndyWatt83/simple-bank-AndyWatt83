03f8eb9a8a8bef6f0b73c1c78a8e51e324e5be6c
Andy Watt <andy.watt83@gmail.com>
2018-12-20 21:34:44 +0000

    Testing Travis build

--- contracts/SimpleBank.sol
+++ contracts/SimpleBank.sol
@@ -13,43 +13,44 @@ contract SimpleBank {
     //
 
     /* Fill in the keyword. Hint: We want to protect our users balance from other contracts*/
-    mapping (address => uint) balances;
+    mapping (address => uint) private balances;
 
     /* Fill in the keyword. We want to create a getter function and allow contracts to be able to see if a user is enrolled.  */
-    mapping (address => bool) enrolled;
+    mapping (address => bool) public enrolled;
 
     /* Let's make sure everyone knows who owns the bank. Use the appropriate keyword for this*/
-    address owner;
+    address public owner;
 
     //
     // Events - publicize actions to external listeners
     //
 
     /* Add an argument for this event, an accountAddress */
-    event LogEnrolled();
+    event LogEnrolled(address indexed accountAddress);
 
     /* Add 2 arguments for this event, an accountAddress and an amount */
-    event LogDepositMade();
+    event LogDepositMade(address indexed accountAddress, uint indexed amount);
 
     /* Create an event called LogWithdrawal */
     /* Add 3 arguments for this event, an accountAddress, withdrawAmount and a newBalance */
-
+    event LogWithdrawal(address indexed accountAddress, uint indexed amount, uint indexed newBalance);
 
     //
     // Functions
     //
 
     /* Use the appropriate global variable to get the sender of the transaction */
-    constructor() {
+    constructor() public {
         /* Set the owner to the creator of this contract */
+        owner = msg.sender;
     }
 
     /// @notice Get balance
     /// @return The balance of the user
     // A SPECIAL KEYWORD prevents function from editing state variables;
     // allows function to run locally/off blockchain
-    function balance() public returns (uint) {
-        /* Get the balance of the sender of this transaction */
+    function balance() public view returns (uint) {
+        return balances[msg.sender];
     }
 
     /// @notice Enroll a customer with the bank
